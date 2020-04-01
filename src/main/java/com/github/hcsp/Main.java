6c46f03528eed2e9ce12467ade13550025aab7b3
linkai.huang <>
2020-04-01 15:33:02 +0800

    代码优化

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,39 +12,95 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
+    private static final String username = "root";
+    private static final String password = "123456";
 
-    public static void main(String[] args) throws IOException {
-        //待处理的链接池
-        List<String> linkPool = new ArrayList<>();
-        //已经处理的链接池
-        Set<String> processLinks = new HashSet<>();
-        linkPool.add("https://sina.cn");
+    private static List<String> loadUrlFromDatabase(Connection connection, String sql) throws SQLException {
+        ResultSet set = null;
+        List<String> results = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            set = statement.executeQuery();
+            while (set.next()) {
+                results.add(set.getString(1));
+            }
+        } finally {
+            if (set != null) {
+                set.close();
+            }
+        }
+        return results;
+    }
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:D:/java/produce/yinghe/xiedaimale-crawler/target/news", username, password);
+
         while (true) {
+            //待处理的链接池
+            //从数据库加载即将处理的链接的代码
+            List<String> linkPool = loadUrlFromDatabase(connection, "select LINK from LINKS_TO_BE_PROCESSED");
             if (linkPool.isEmpty()) {
                 break;
             }
-            //remove方法会返回删掉的那个元素
+            //从待处理池子种拿一个出来
             String link = linkPool.remove(linkPool.size() - 1);
+            //处理完后从池子和数据库种删除
+            insertLinkIntoDatabase(connection, link, "delete from LINKS_TO_BE_PROCESSED where  LINK = ?");
 
-            if (processLinks.contains(link)) {
+            //询问数据库当前链接有么有被处理过
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
+            //关系news.sina.cn
             if (isInterestingLink(link)) {
-                Document doc= httpGetAndParseHtml(link);
-                doc.select("a").stream().map(aTag-> aTag.attr("href")).forEach(linkPool::add);
+                Document doc = httpGetAndParseHtml(link);
+
+                parseUrlsFromPageAndStoneIntoDatabase(connection, doc);
+                //如果是新闻页面，那就存入数据库
                 stoneIntoDatabaseIfItIsNewsPage(doc);
-                processLinks.add(link);
-            } else {
-                //不用管的方法
+
+                insertLinkIntoDatabase(connection, link, "insert into LINKS_ALREADY_PROCESSED (LINK) values (?)");
             }
         }
     }
+
+    private static void parseUrlsFromPageAndStoneIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            insertLinkIntoDatabase(connection, href, "insert into LINKS_TO_BE_PROCESSED (LINK) values (?)");
+        }
+    }
+
+    //询问数据库当前链接有么有被处理过
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet set = null;
+        try (PreparedStatement statement = connection.prepareStatement("select LINK from LINKS_ALREADY_PROCESSED WHERE  LINK = ?")) {
+            statement.setString(1, link);
+            set = statement.executeQuery();
+            while (set.next()) {
+                return true;
+            }
+        } finally {
+            if (set != null) {
+                set.close();
+            }
+        }
+        return false;
+    }
+
+    //处理完后从池子和数据库种删除
+    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
 
     private static void stoneIntoDatabaseIfItIsNewsPage(Document doc) {
         ArrayList<Element> articleTags = doc.select("article");
@@ -75,7 +132,7 @@ public class Main {
     }
 
     private static boolean isInterestingLink(String link) {
-        return (isNewsPage(link) || "https://sina.cn".equals(link)) && isNotLoginPage(link);
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
     }
 
     private static boolean isIndexPage(String link) {
